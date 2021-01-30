396e30b327cd7e25519ea03043f9bceba1e0dcab
HerbCaudill <herb@devresults.com>
2021-01-30 15:31:39 +0100

    pages/posts/[id].tsx:

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -4,37 +4,42 @@ import Head from 'next/head'
 import Date from '../../components/Date'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { PostData } from '../../lib/types'
-import { markdownToHtml } from '../../lib/markdownToHtml'
 
-const Post = ({ postData }: { postData: PostData }) => (
-  <Layout header={<></>}>
-    <Head>
-      <title>{postData.title}</title>
-    </Head>
-    <article>
-      <h1>{postData.title}</h1>
-      <div>
-        <Date dateString={postData.date} />
-      </div>
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </article>
-  </Layout>
-)
+const Post = ({
+  postData: { title, subtitle, image, date, contentHtml },
+}: {
+  postData: PostData
+}) => {
+  const header = (
+    <div>
+      <img src={image} className="w-full h-40 object-cover" />
+    </div>
+  )
+
+  return (
+    <Layout header={header}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <article>
+        <h1 className="font-serif font-light text-5xl">{title}</h1>
+        <h2 className="font-bold text-2xl">{subtitle}</h2>
+        <div className="font-mono text-sm tracking-widest mt-12 mb-6">
+          <Date dateString={date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
 
 export default Post
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const postData = post(params.id as string)
-  const contentHtml = await markdownToHtml(postData.content)
-  return {
-    props: {
-      postData: {
-        ...postData,
-        contentHtml,
-      },
-    },
-  }
-}
+export const getStaticProps: GetStaticProps = async ({ params }) => ({
+  props: {
+    postData: post(params.id as string),
+  },
+})
 
 export const getStaticPaths: GetStaticPaths = async () => ({
   paths: getAllPostIdParams(),
