3924ffab0614ac620ec4d22ffff2f1c5cc66f27f
HerbCaudill <herb@devresults.com>
2021-01-30 15:31:29 +0100

    marked

--- lib/markdownToHtml.tsx
+++ lib/markdownToHtml.tsx
@@ -1,8 +1,3 @@
-import remark from 'remark'
-import html from 'remark-html'
-
-export const markdownToHtml = async (markdown: string): Promise<string> => {
-  // Use remark to convert markdown into HTML string
-  const processedContent = await remark().use(html).process(markdown)
-  return processedContent.toString()
-}
+import marked from 'marked'
+export const markdownToHtml = (markdown: string) =>
+  markdown ? marked(markdown) : ''
