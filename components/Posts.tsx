2d0b6af94783012b663ed686ccba8d8105e5000a
HerbCaudill <herb@devresults.com>
2021-10-11 19:18:53 +0200

    components/Posts.tsx:

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -2,9 +2,13 @@ import { PostData } from 'lib/types'
 import { PostLink } from './PostLink'
 
 export const Posts: React.FC<{ posts: PostData[] }> = ({ posts }) => {
+  const includeDrafts = false
+  const filteredPosts = includeDrafts
+    ? posts // no filter if we're showing drafts
+    : posts.filter(postData => postData.draft !== true) // otherwise filter drafts out
   return (
     <>
-      {posts.map(postData => (
+      {filteredPosts.map(postData => (
         <PostLink key={postData.id} {...postData} />
       ))}
     </>
