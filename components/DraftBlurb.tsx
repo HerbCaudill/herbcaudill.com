7a4a2e6bcc3f71df938fd6fd48471c428d561fdb
HerbCaudill <herb@devresults.com>
2021-10-11 19:34:34 +0200

    components/DraftBlurb.tsx:

--- components/DraftBlurb.tsx
+++ components/DraftBlurb.tsx
@@ -4,7 +4,15 @@ export const DraftBlurb: React.FC<{ draft: boolean; className?: string }> = ({
 }) =>
   draft ? (
     <div className={`border-2 border-red-700 p-3 text-red-700 font-mono text-sm ${className}`}>
-      <b className="block">DRAFT ARTICLE</b> Thanks for prereading this post! Please don't share it
-      widely until I've published it.
+      <b className="block">DRAFT ARTICLE</b> Thanks for prereading this post! I'd love to know what
+      you think — feel free to message me at{' '}
+      <a
+        target="_blank"
+        href={`https://twitter.com/messages/compose?recipient_id=16202589&text=article+feedback`}
+        data-screen-name="@herbcaudill"
+      >
+        @herbcaudill
+      </a>
+      . Please don't share it widely until I've published it.
     </div>
   ) : null
