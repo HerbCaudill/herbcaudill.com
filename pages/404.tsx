7df67251c12abd5ab0caa600a64ac0e957aeedc0
HerbCaudill <herb@devresults.com>
2022-09-12 15:00:26 +0200

    pages/404.tsx:

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,19 +2,13 @@ import { IndexLayout } from 'components/IndexLayout'
 
 const NotFoundPage: React.FC = () => {
   return (
-    <IndexLayout label="Oops">
+    <IndexLayout label="404">
       <article>
         <div className="border border-black pt-G lg:p-G text-center">
-          <h1 className="text-yuge font-sans font-bold text-black tracking-tighter mt-0 ">
-            404
-          </h1>
           <h3 className="text-3xl md:text-4xl lg:text-6xl font-serif font-light mt-2 tracking-tighter mb-10">
             Page not found.
           </h3>
-          <div
-            className="relative overflow-hidden"
-            style={{ paddingBottom: '90%' }}
-          >
+          <div className="relative overflow-hidden" style={{ paddingBottom: '90%' }}>
             <iframe
               className="absolute "
               style={{
