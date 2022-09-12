1025b11deabd8e6e251c8425cea7addb8ec32dc5
HerbCaudill <herb@devresults.com>
2022-09-12 15:00:36 +0200

    pages/words/index.tsx:

--- pages/words/index.tsx
+++ pages/words/index.tsx
@@ -1,21 +1,23 @@
-import { GetStaticProps } from 'next'
+import { IndexLayout } from 'components/IndexLayout'
 import { Posts } from 'components/Posts'
 import { posts } from 'lib/posts'
 import { PostData } from 'lib/types'
-import { IndexLayout } from 'components/IndexLayout'
+import { GetStaticProps } from 'next'
 
 const WordsIndex: React.FC<{ posts: PostData[] }> = ({ posts }) => {
   return (
     <IndexLayout label="Words">
-      <Posts posts={posts} />
+      <Posts posts={posts} includeDrafts={false} />
     </IndexLayout>
   )
 }
 
 export default WordsIndex
 
-export const getStaticProps: GetStaticProps = async () => ({
-  props: {
-    posts: posts(),
-  },
-})
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      posts: posts(),
+    },
+  }
+}
