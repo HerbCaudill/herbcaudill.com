3a55c4dd260a2cd322ffd942447039861f205c4c
HerbCaudill <herb@devresults.com>
2022-09-12 15:00:03 +0200

    components/DraftBlurb.tsx:

--- components/DraftBlurb.tsx
+++ components/DraftBlurb.tsx
@@ -1,18 +1,18 @@
-export const DraftBlurb: React.FC<{ draft: boolean; className?: string }> = ({
-  draft,
-  className = '',
-}) =>
+export const DraftBlurb: React.FC<{ draft: boolean; className?: string }> = ({ draft, className = '' }) =>
   draft ? (
-    <div className={`border-2 border-red-700 p-3 text-red-700 font-mono text-sm ${className}`}>
-      <b className="block">DRAFT ARTICLE</b> Thanks for prereading this post! I'd love to know what
-      you think — feel free to message me at{' '}
-      <a
-        target="_blank"
-        href={`https://twitter.com/messages/compose?recipient_id=16202589&text=article+feedback`}
-        data-screen-name="@herbcaudill"
-      >
-        @herbcaudill
-      </a>
-      . Please don't share it widely until I've published it.
+    <div className={`border-4 grid grid-cols-7 border-red-700  text-red-700 font-sans font ${className}`}>
+      <div className="p-2 col-span-3 text-3xl font-extrabold border-r border-red-700">DRAFT</div>
+      <div className="p-2 col-span-4 text-sm">
+        Thanks for prereading this post! I'd love to know what you think — feel free to DM me at{' '}
+        <a
+          className="font-semibold hover:underline"
+          target="_blank"
+          href={`https://twitter.com/messages/compose?recipient_id=16202589&text=article+feedback`}
+          data-screen-name="@herbcaudill"
+        >
+          @herbcaudill
+        </a>
+        . Please don't share it widely until I've published it.
+      </div>
     </div>
   ) : null
