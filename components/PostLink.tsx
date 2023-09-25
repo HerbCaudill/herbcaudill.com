d0569d1c3fd46944832c2fefee1c331b42f82765
HerbCaudill <herb@devresults.com>
2023-09-25 18:55:35 +0200

    artificially trigger click on PostLink

--- components/PostLink.tsx
+++ components/PostLink.tsx
@@ -1,55 +1,58 @@
-﻿import Link from 'next/link'
-import { Date } from './Date'
-import { PostMetadata } from 'lib/types'
-import { Tag } from './Tag'
+﻿import { PostMetadata } from 'lib/types'
+import Link from 'next/link'
+import { useRef } from 'react'
 
 const env = process.env.NODE_ENV
 const isProd = env === 'production'
 
-export const PostLink = ({ id, date, title, subtitle, description, draft, thumbnail }: PostLinkProps) => (
-  <Link legacyBehavior href={`/words/${id}`} key={id}>
+export const PostLink = ({ id, date, title, subtitle, description, draft, thumbnail }: PostLinkProps) => {
+  const link = useRef<HTMLAnchorElement>(null)
+  return (
     <div
       className={`
-        grid gap-G
-        grid-cols-4 md:grid-cols-9
-        border-t border-gray-400
-        pt-2 pb-7
-        group
-        hover:bg-gray-400 hover:bg-opacity-5 transition-all duration-500 ease-in-out
-        cursor-pointer
-        ${isProd && draft ? 'opacity-25' : ''}
-      `}
+          PostLink
+          grid gap-G
+          grid-cols-4 md:grid-cols-9
+          border-t border-gray-400
+          pt-2 pb-7
+          hover:bg-gray-400 hover:bg-opacity-5 transition-all duration-500 ease-in-out
+          cursor-pointer
+          ${isProd && draft ? 'opacity-25' : ''}
+        `}
+      onClick={_ => {
+        link.current.click()
+      }}
     >
       <div className="col-span-1 md:col-span-3">
         <div className="w-full relative overflow-hidden pb-100% md:pb-50% mb-2">
           <img className="absolute w-full h-full object-cover border" src={thumbnail} />
         </div>
       </div>
-
       <div className="col-span-3">
-        <a
-          className="font-sans font-bold text-lg tracking-tight leading-none md:leading-none ink-underline-on-hover"
+        <Link
+          href={`/words/${id}`}
+          ref={link}
+          key={id}
+          className="font-sans font-bold text-lg tracking-tight leading-none md:leading-none "
           dangerouslySetInnerHTML={{ __html: title }}
         />
         <div
           className="font-mono leading-tight md:leading-tight py-1 text-xs md:font-serif md:text-base"
           dangerouslySetInnerHTML={{ __html: subtitle }}
         />
-        <Date dateString={date} className="hidden mt-3 md:block text-2xs tracking-widest font-mono uppercase" />
+        {/* <Date dateString={date} className="hidden mt-3 md:block text-2xs tracking-widest font-mono uppercase" /> */}
       </div>
-
       <div
         className="hidden md:block col-span-3 text-2xs lg:text-xs text-gray-700 font-mono"
         dangerouslySetInnerHTML={{ __html: description }}
       />
-
       {/* <div className="hidden lg:block">
-        {tags.map(tag => (
-          <Tag key={tag}>{tag}</Tag>
-        ))}
-      </div> */}
+          {tags.map(tag => (
+            <Tag key={tag}>{tag}</Tag>
+          ))}
+        </div> */}
     </div>
-  </Link>
-)
+  )
+}
 
 interface PostLinkProps extends PostMetadata {}
